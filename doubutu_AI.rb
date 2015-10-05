ab98c0ba075970233ef7aaaaf9bf2de301e9be20
fono09 <fono@fono.jp>
2015-10-05 21:59:15 +0900

    Define super class Piece

--- doubutu_AI.rb
+++ doubutu_AI.rb
@@ -1,95 +1,153 @@
 require 'socket'
+require 'matrix'
 
 
 
 p default_settings
 exit
+INITIAL_POSITION = {
+	["c",1] => [1,2],
+	["c",2] => [1,1],
+	["e",1] => [1,3],
+	["e",2] => [2,1],
+	["g",1] => [2,3],
+	["g",2] => [0,0],
+	["l",1] => [1,3],
+	["l",2] => [1,0]
+}
+#初期位置に関して
+
+MOVE_DIRECTION = {
+	"c" => [false,true,false,
+		false,false,false,
+		false,false,false],
+		"e" => [false,true,false,
+			true,false,true,
+			false,true,false],
+			"g" => [true,false,true,
+				false,false,false,
+				true,false,true],
+				"l" => [true,true,true,
+					true,false,true,
+					true,true,true],
+					"h" => [true,true,true,
+						true,false,true,
+						false,true,false]
+}
+#時計回りに進行方向
+
+
+class Board
+
+end
 
 class Piece
-	INITIAL_POSITION = {
-		["c",1] => [1,2],
-		["c",2] => [1,1],
-		["e",1] => [1,3],
-		["e",2] => [2,1],
-		["g",1] => [2,3],
-		["g",2] => [0,0],
-		["l",1] => [1,3],
-		["l",2] => [1,0]
-	}
-	#初期位置に関して
-	
-	MOVE_DIRECTION = {
-		"c" => [true,false,false,false,false,false,false,false],
-		"e" => [false,true,false,true,false,true,false,true],
-		"g" => [true,false,true,false,true,false,true,false],
-		"l" => [true,true,true,true,true,true,true,true],
-		"h" => [true,true,true,false,true,false,true,true]
-	}
-	#時計回りに進行方向
 
-	def initialize(type,player,pos_x,pos_y)
-		if INITIAL_POSITION[[type,player]] != [pos_x,pos_y] then
-			raise "Piece#initialize failed"
-		end
-		@type = type
-		@player = player
-		@pos_x = pos_x
-		@pos_y = pos_y
+	def initialize(p,m)
+		@player = p
+		@mv_dir = m
 	end
 
-	def mv(dst_x,dst_y)
-		
+	def vec_2_idx(vx,vy)
+		return 1-vy,vx+1
+	end
+
+	def movable?(x,y)
+		idx = vec_2_idx(x,y)
+		dir = @player==1 ? @mv_dir : @mv_dir.reverse
+		return dir[idx[0]][idx[1]] 
 	end
 
 end
 
-serverAddr = "localhost"
-serverPort = 4444
+class Chicken < Piece
+	def initialize(p)
+		super(p,[false,true,false,
+			false,false,false,
+			false,false,false])
+	end
 
-s = TCPSocket.open(serverAddr,serverPort)
+	def promotion
+		@mv_dir = [true,true,true,
+			true,false,true,
+			false,true,false]
+	end
+end
 
-puts s.gets
-Thread.abort_on_exception = true
+class Elephant < Piece
+	def initialize(p)
+		super(p,[true,false,true,
+		      false,false,false,
+		      true,false,true])
+	end
+end
 
-t = Thread.new do
+class Giraf < Piece
+	def initialize(p)
+		super(p,[false,true,false,
+		      true,false,true,
+		      false,true,false])
+	end
+end
 
-	while line = s.gets
-		p line
+class Lion < Piece
+	def initialize(p)
+		super(p,[true,true,true,
+		      true,false,true,
+		      true,true,true])
+	end
+end
 
-		if line =~ /, / then
-			board=[]
-			line.chomp.split(/, /).map{|piece| 
-				piece.split(/ /) 
-			}.each{|piece|
-				i = piece[0][0].codepoints[0].to_i - 'A'.codepoints[0].to_i
-				j = piece[0][1].to_i - 1
-				unless board[i] then
-					board[i] = []
-				end
-				board[i][j] = piece[1]
-			}
-			p board
+		
+
+
+	serverAddr = "localhost"
+	serverPort = 4444
+
+	s = TCPSocket.open(serverAddr,serverPort)
+
+	puts s.gets
+	Thread.abort_on_exception = true
+
+	t = Thread.new do
+
+		while line = s.gets
+			p line
+
+			if line =~ /, / then
+				board=[]
+				line.chomp.split(/, /).map{|piece| 
+					piece.split(/ /) 
+				}.each{|piece|
+					i = piece[0][0].codepoints[0].to_i - 'A'.codepoints[0].to_i
+					j = piece[0][1].to_i - 1
+					unless board[i] then
+						board[i] = []
+					end
+					board[i][j] = piece[1]
+				}
+				p board
+			end
+			sleep 0.1
 		end
+	end	
+	t.run
+
+	while true
+
+		line = readline
+
+		if line =~ /^q\s*$/ then
+			break
+		end
+
+		s.write(line)
+
 		sleep 0.1
-	end
-end	
-t.run
 
-while true
-
-	line = readline
-
-	if line =~ /^q\s*$/ then
-		break
 	end
 
-	s.write(line)
+	puts "bye"
 
-	sleep 0.1
-		
-end
-
-puts "bye"
-
-s.close
+	s.close
 
